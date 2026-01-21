de990421a00c8c4083ba042e40badd9e6559ed63
eejon <eejonlee@gmail.com>
2026-01-21 14:02:58 +0800

    Rename Duke bot to Athena

---
+++ src/main/java/Athena.java
@@ -0,0 +1,13 @@
+public class Athena {
+    /* Class Level Constants */
+    private static final String LINE_BREAK = "\t____________________________________________________________\n";
+    private static final String LOGO =
+          "\t    ___  __  __                \n"
+        + "\t   / _ |/ /_/ /  ___ ___  ___ _\n"
+        + "\t  / __ / __/ _ \\/ -_) _ \\/ _ `/\n"
+        + "\t /_/ |_\\__/_//_/\\__/_//_/\\_,_/ \n";
+
+    public static void main(String[] args) {
+        
+    }
+}
