2338d1bcc43ddfe2a2f23db8e0225112c9f61e83
eejon <eejonlee@gmail.com>
2026-01-21 14:07:56 +0800

    Add greeting and exit

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -7,7 +7,21 @@ public class Athena {
         + "\t  / __ / __/ _ \\/ -_) _ \\/ _ `/\n"
         + "\t /_/ |_\\__/_//_/\\__/_//_/\\_,_/ \n";
 
+    private static void printGreeting() {
+        StringBuilder sb = new StringBuilder(LINE_BREAK).append(LOGO).append(LINE_BREAK);
+        sb.append("\n\t Greetings! I am Athena!\n\t What's on your mind?\n").append(LINE_BREAK);
+        System.out.println(sb);
+    }
+
+    private static void printExit() {
+        System.out.println("\t Until next time.");
+        System.out.println(LINE_BREAK);
+    }
+
     public static void main(String[] args) {
-        
+        // Greetings
+        printGreeting();
+        // Exit
+        printExit();
     }
 }
