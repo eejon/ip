b26244bdc63cdeb15deaa2e1bf4b47238e6c628f
eejon <eejonlee@gmail.com>
2026-01-21 16:36:56 +0800

    Add Todo class

---
+++ src/main/java/Todo.java
@@ -0,0 +1,25 @@
+/**
+ * Represents a todo task without any date/time attached to it
+ * Subclass of Task
+ */
+public class Todo extends Task {
+	 /**
+     * Creates a new Todo task.
+     *
+     * @param description The description of the todo task.
+     */
+    public Todo(String description) {
+        super(description);
+    }
+
+    /**
+     * Returns a string representation of todo task.
+     * Format: [T][status_icon] description
+     *
+     * @return String representation of todo task.
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
