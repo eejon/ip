4af76c5d55d765c6f42e32e50f2ef81adc1f7e74
eejon <eejonlee@gmail.com>
2026-01-21 19:38:27 +0800

    Add custom exception for user inputs

---
+++ src/main/java/AthenaException.java
@@ -0,0 +1,15 @@
+/**
+ * Represents exceptions specific to the Athena.
+ * Used to handle errors such as incorrect user input
+ */
+public class AthenaException extends Exception {
+    
+    /**
+     * Creates a new AthenaException with the specified error message.
+     *
+     * @param message The error message explaining what went wrong.
+     */
+    public AthenaException(String message) {
+        super(message);
+    }
+}
