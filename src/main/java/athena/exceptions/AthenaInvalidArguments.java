30e23b453d44677759003b7883d0f81559a48292
eejon <eejonlee@gmail.com>
2026-01-28 21:31:14 +0800

    chore: Comply with coding standards

--- src/main/java/athena/exceptions/AthenaInvalidArguments.java
+++ src/main/java/athena/exceptions/AthenaInvalidArguments.java
@@ -8,11 +8,11 @@ public class AthenaInvalidArguments extends AthenaException {
 
     /* Class level constant error messages */
     private static final String INVALID_TODO = 
-    "\t Strategy requires detail. A todo must have a defined objective.";
+        "\t Strategy requires detail. A todo must have a defined objective.";
     private static final String INVALID_DEADLINE = 
-    "\t Strategy requires detail. A deadline must have a defined objective.";
+        "\t Strategy requires detail. A deadline must have a defined objective.";
     private static final String INVALID_EVENT = 
-    "\t Strategy requires detail. An event must have a defined objective.";
+        "\t Strategy requires detail. An event must have a defined objective.";
 
     private AthenaInvalidArguments(String message) {
         super(message);
