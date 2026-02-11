538b318e47217c194560765eb53b3eabbc9e7b89
eejon <eejonlee@gmail.com>
2026-02-12 04:21:58 +0800

    chore: Remove trailing whitespace

--- src/main/java/athena/exceptions/AthenaInvalidDate.java
+++ src/main/java/athena/exceptions/AthenaInvalidDate.java
@@ -8,7 +8,7 @@ public class AthenaInvalidDate extends AthenaException {
     /* Class level constant error messages */
     private static final String INVALID_DATE =
         "\t The fates do not recognize this date. Speak in the tongue of yyyy-MM-dd.";
-    private static final String FROM_DATE_GREATER_THAN_TO_DATE = 
+    private static final String FROM_DATE_GREATER_THAN_TO_DATE =
         "\t Wisdom dictates that one cannot finish before they have begun. Adjust your date range accordingly.";
 
     private AthenaInvalidDate(String message) {
